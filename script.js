760e8f7dac58853f58405b5d4189852172739ee8
unknown <you@example.com>
2018-10-28 00:32:02 +0300

    edit script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//Burger-menu
+
+$(".button-menu").click(function() {
+    $(this).closest(".mob-menu").toggleClass("active");
+});
+
+
+//Select
+
 var x, i, j, selElmnt, a, b, c;
 /*look for any elements with the class "custom-select":*/
 x = document.getElementsByClassName("custom-select");
@@ -71,3 +80,46 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+
+//Slider
+
+var slideIndex = 1;
+var timer = null;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n == undefined) {
+        n = ++slideIndex
+    }
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    timer = setTimeout(showSlides, 3000);
+}
